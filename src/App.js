a7d88bfc096adeea6c5fd52580b26940c04277ab
tsigemit <tsigabum@gmail.com>
2020-07-03 16:54:37 +0200

    Menu component part2 using sate and props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 //import './App.css';
 
-function App() {
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+  render(){
   return (
    <div>
      <Navbar dark color="primary">
@@ -15,9 +23,10 @@ function App() {
        </div>
      </Navbar>
 
-     <Menu />
+      <Menu dishes={this.state.dishes} />
    </div>
   );
+}
 }
 
 export default App;
