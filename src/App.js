4e26098ea216cddb79e216ec4170b05defa8b0e0
tsigemit <tsigabum@gmail.com>
2020-07-03 15:30:16 +0200

    Menu Component part one is added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import {Navbar, NavbarBrand} from 'reactstrap';
-import './App.css';
+import Menu from './components/MenuComponent';
+//import './App.css';
 
 function App() {
   return (
-   <div className="App">
+   <div>
      <Navbar dark color="primary">
        <div className="container" >
          <NavbarBrand href="/">
-           This is Reac App.
+           Cafe and Restauran Management System.
          </NavbarBrand>
        </div>
      </Navbar>
+
+     <Menu />
    </div>
   );
 }
