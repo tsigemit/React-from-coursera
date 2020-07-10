0e725ce3b4a2daf47aaea3cd05258cf7781030a3
tsigemit <tsigabum@gmail.com>
2020-07-10 22:33:58 +0200

    SPA using route on all the components

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,15 +5,30 @@ import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Contact from './ContactComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
+
 
 class Main extends Component {
+    constructor(props) {
+        super(props);
 
+        this.state = {
+            dishes: DISHES,
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
+        };
+    }
     render() {
         const HomePage = () => {
             return (
-                <Home
-                />
+                <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                      promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                      leader={this.state.leaders.filter((leader) => leader.featured)[0]}   />
             );
         }
     return (
@@ -23,8 +38,9 @@ class Main extends Component {
                 <Header />
             <Switch>
                 <Route path='/home' component={HomePage} />
-                <Route exact path='/menu' component={() => <Menu dishes={DISHES} />} />
-                <Redirect to="/home" />
+                <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+                <Route exact path='/contactus' component={Contact} />
+                <Redirect to="/home" />                
             </Switch>
                 <Footer />
             </div>
